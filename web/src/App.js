8a8f314eb454ecb9200da3b5054665356bbaa1fd
Yuri Alves <yurialves2@gmail.com>
2020-01-18 17:10:09 -0300

    created aside form

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+import './Main.css';
+import './Sidebar.css';
+
 function App() {
+
   return (
-    <div className="App">
-      Olá, Mundo
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          {/* Github username */}
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          {/* Tecnologias */}
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+          {/* Latitude / Longitude */}
+          <div className="input-group">
+            {/* Latitude */}
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            {/* Longitude */}
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude"/>
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
     </div>
   );
 }
