d41770b93054b99910ad33f364ae9230bbc17bea
Yuri Alves <yurialves2@gmail.com>
2020-01-18 19:32:26 -0300

    added cors to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config({ path: '.env' });
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
@@ -11,6 +12,7 @@ mongoose.connect(getMongoURI(), {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
