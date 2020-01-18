52b48c3548dc25357b5f8d6dc9cdc286b93b3985
Yuri Alves <yurialves2@gmail.com>
2020-01-18 19:33:14 -0300

    making requests to backend

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -7,34 +8,99 @@ import './Sidebar.css';
 
 import avatar from './avatar.svg';
 
+
 function App() {
+  const [devs, setDevs] = useState([]);
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [github_username, setGithubUsername] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { coords: { latitude, longitude } } = position;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (error) => console.error(error)
+    );
+  }, []);
+
+  useEffect(() => {
+    (async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+      console.log(devs);
+    })();
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+    const response = await api.post('/dev', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    });
+
+    console.log(response.data);
+
+    setGithubUsername('');
+    setTechs('');
+    setLatitude('');
+    setLongitude('');
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          {/* Github username */}
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              type="text"
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
-          {/* Tecnologias */}
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input
+              type="text"
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
-          {/* Latitude / Longitude */}
           <div className="input-group">
-            {/* Latitude */}
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input
+                type="text"
+                name="latitude"
+                id="latitude"
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
-            {/* Longitude */}
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude"/>
+              <input
+                type="text"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
           <button type="submit">Salvar</button>
---
+++ web/src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3333'
+    : 'http://localhost:3333'
+});
+
+export default api;
