70a52d82887c50047c140be65b24c8e5ae801a6b
Yuri Alves <yurialves2@gmail.com>
2020-01-26 20:07:08 -0300

    created routes and pages

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/Routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Olá, Mundo!</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#7159c1',
-  },
-  title: {
-    fontSize: 32,
-    color: 'white',
-    fontWeight: 'bold',
-  }
-});
