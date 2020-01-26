c22f0b8f2b479da67ec4ebd559ba170bec031d0a
Yuri Alves <yurialves2@gmail.com>
2020-01-26 19:44:46 -0300

    Hello world mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Olá, Mundo!</Text>
     </View>
   );
 }
@@ -12,8 +12,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#7159c1',
   },
+  title: {
+    fontSize: 32,
+    color: 'white',
+    fontWeight: 'bold',
+  }
 });
