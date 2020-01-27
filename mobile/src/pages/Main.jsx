3b9c240a80786ac689eae8372b91cfcaf336fba6
Yuri Alves <yurialves2@gmail.com>
2020-01-26 22:14:56 -0300

    created search input

--- mobile/src/pages/Main.jsx
+++ mobile/src/pages/Main.jsx
@@ -1,8 +1,8 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
-
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 
 export function Main({ navigation: { navigate } }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -49,7 +49,20 @@ export function Main({ navigation: { navigate } }) {
           </Callout>
         </Marker>
       </MapView>
-      <View style={styles.searchForm}></View>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+        <TouchableOpacity
+          style={styles.loadButton}
+          onPress={() => {}}>
+          <MaterialIcons name="my-location"size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
     </>
   )
 }
@@ -80,6 +93,36 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   searchForm: {
-
-  }
-})
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#fff',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 1,
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8d4eff',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
+  },
+});
