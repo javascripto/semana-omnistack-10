dd99a575d4850bf3cadfc75cdd5e544fe2104fa2
Yuri Alves <yurialves2@gmail.com>
2020-01-26 21:30:51 -0300

    created Map and navigating to Profile

--- mobile/src/pages/Main.jsx
+++ mobile/src/pages/Main.jsx
@@ -1,8 +1,85 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-export function Main() {
+
+export function Main({ navigation: { navigate } }) {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+      if (granted) {
+        const { coords: { latitude, longitude} } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.2,
+          longitudeDelta: 0.2,
+        });
+      }
+    }
+    loadInitialPosition();   
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
   return (
-    <View />
+    <>
+      <MapView
+        style={styles.map}
+        initialRegion={currentRegion}>
+        <Marker
+          coordinate={{ latitude: -23.6493986, longitude: -46.7730587 }}>
+          <Image
+            style={styles.avatar}
+            source={{ uri: 'https://avatars3.githubusercontent.com/u/16804522?s=460&v=4'}}
+          />
+          <Callout onPress={() => navigate('Profile', { github_username: 'javascripto' })}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Yuri Almeida</Text>
+              <Text style={styles.devBio}>Não Tenho Bio aqui</Text>
+              <Text style={styles.devTechs}>Angular, Node.js, ReactJS, PHP, Python, Kotlin</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}></View>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#fff',
+  },
+  callout: {
+    width: 260,
+  },
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+  devTechs: {
+    marginTop: 5,
+  },
+  searchForm: {
+
+  }
+})
--- mobile/src/pages/Profile.jsx
+++ mobile/src/pages/Profile.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export function Profile() {
+export function Profile({ navigation: { getParam }}) {
+  const github_username = getParam('github_username');
   return (
-    <View />
+    <WebView
+      style={{ flex: 1 }}
+      source={{ uri: `https://github.com/${github_username}`}}
+    />
   )
 }
